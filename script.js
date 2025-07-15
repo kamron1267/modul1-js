ac10a9c72e2ae10b244504172dbc61b37c6ab0fb
kamron1267 <begzodabdujabborvij@gmail.com>
2025-07-16 01:39:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log(2025)
+console.log("jonkalarim privet")
+
+var name = "Alex";
+let age = 25;
+const PI = 3.14; 
+
+
+let userName = "Alex";
+let productPrice = 5000;
+
+
+let ism = prompt("ism");
+alert("salom," + ism);
+console.log("ISM:, " + ism);
+
+let familiya = prompt("familiya");
+alert("familiya," + familiya); 
+console.log("FAMILIYA:, " + familiya);
+
+let yosh = prompt("yosh");
+alert("yosh," + yosh);
+console.log("YOSH:, " + yosh);
+
+let joylashuv = prompt("qatta turasan");
+alert("joylashuv," + joylashuv);
+console.log("ADRES, " + joylashuv);
+
+let mkraqam = prompt("maktab raqam");
+alert("mk raqam," + mkraqam);
+console.log("MAKTAB RAQAMI, " + mkraqam);
+
+let frendname = prompt("ortogini ismi");
+alert("ortogini ismi," + frendname);
+console.log("DOSTINI ISMI, " + frendname);
+
+let adeniism = prompt("adeni ismi");
+alert("adeni ismi," + adeniism);
+console.log("ADENI ISMI, " + adeniism);
+
+let oyiniismi = prompt("oyini ismi");
+alert("oyini ismi," + oyiniismi);
+console.log("OYINI ISMI, " + oyiniismi);
+
+let jonkeniismi = prompt("qizini ismi");
+alert("qizini ismi," + jonkeniismi);
+console.log("QIZINI ISMI, " + jonkeniismi);
+
+let sovol = prompt("jonkaligini uyindigila biladimi ");
+alert("jonkaligini uyindigila biladimi ," + sovol);
+console.log("SOVOLGA JAVOB, " + sovol);
